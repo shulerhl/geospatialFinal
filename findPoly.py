948b240f0b9cd9c67e76f67e253f32a5a2e06268
kmads <kamaddio@gmail.com>
2016-03-08 16:16:56 -0600

    Connects all segments, better identifies dashed lane line, better removal of horizon lines.

--- findPoly.py
+++ findPoly.py
@@ -9,49 +9,48 @@ from classProject import testPlot, getSegmentsWithMatchingAngleAndPos, construct
 
 
 def houghT(input_img, run_img, img_edges, h_array):
-    h_lines = cv2.HoughLinesP(img_edges, 1, np.pi / 180, 80, 30000, 100)
-    if h_lines.size == 0:
-        print("Hough found no lines")
-    else:
-        # [r,c] = input_img.shape()
-        print("LINES SIZE", h_lines.shape)
-        print(str(h_lines))
+	h_lines = cv2.HoughLinesP(img_edges, 1, np.pi/180, 80, 30000, 45)
+	if h_lines.size == 0:
+		print("Hough found no lines")
+	else:
+		#[r,c] = input_img.shape()
+		print("LINES SIZE", h_lines.shape)
+		print(str(h_lines))
 
-    h_lines = removeHoriz(h_lines)
-    h_lines = makeLines(h_lines)
-    # print h_lines
-    for x1, y1, x2, y2 in h_lines[0]:
-        cv2.line(h_array, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-    cv2.imwrite('houghlines3.jpg', h_array)
-    return h_lines
+	h_lines = removeHoriz(h_lines)
+	h_lines = makeLines(h_lines)
+	# print h_lines
+	for h in h_lines:
+		for x1,y1,x2,y2 in h:
+		    cv2.line(h_array,(x1,y1),(x2,y2),(0,255,0),2)
+	cv2.imwrite('houghlines3.jpg', h_array)
+	return h_lines
 
 
 def removeHoriz(houghL):
-    h_lines = houghL
-    length = len(h_lines)
-    print("SHAPE")
-    print(h_lines.shape)
-    good_lines = []
-    # xHalf = 2048/2  #change 2048 to image size dimensions
+	h_lines = houghL[0]
+	length = len(h_lines)
+	print("SHAPEEE")
+	print(h_lines.shape)
+	good_lines = []
+	#xHalf = 2048/2  #change 2048 to image size dimensions
 
-    for i in range(0, length):
-        currSeg = h_lines[i][0]
-        print("YOOOOO")
-        print(str(currSeg))
-        currAngle = getAngle(currSeg[0], currSeg[1], currSeg[2], currSeg[3])
-        currDist = math.sqrt(math.pow((currSeg[2] - currSeg[0]), 2) + math.pow((currSeg[2] - currSeg[0]), 2))
-        print("CURR ANGLE", str(currAngle))
-        if (abs(currAngle) > np.pi / 12):
-            good_lines.append(h_lines[i][0])
-    return np.array(good_lines)
+	for i in range(0,length):
+		currSeg = h_lines[i]
+		print("YOOOOO")
+		print(str(currSeg))
+		currAngle = getAngle(currSeg[0], currSeg[1], currSeg[2], currSeg[3])
+		currDist = math.sqrt(math.pow((currSeg[2] - currSeg[0]) , 2) + math.pow((currSeg[2] - currSeg[0]), 2))
+		print("CURR ANGLE", str(currAngle))
+		if (abs(currAngle) > math.pi/12):
+			good_lines.append(h_lines[i])
+	return np.array(good_lines)
 
 
 def getAngle(x1, y1, x2, y2):
-    xdiff = x2 - x1
-    ydiff = y2 - y1
-    return math.atan(ydiff / xdiff)
-
+	xdiff = x2 - x1
+	ydiff = y2 - y1
+	return math.atan(float(ydiff)/xdiff)
 
 def makeLines(h_lines):
     lines_segments = getSegmentsWithMatchingAngleAndPos(h_lines)
